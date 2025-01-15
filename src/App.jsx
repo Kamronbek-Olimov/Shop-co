73aaaf1fcf477279b824dc35948dafeab9e37362
Kamronbek Olimov <qobiljonovichkamronbek@gmail.com>
2025-01-15 18:24:27 +0500

    dfdg

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import TopNav from "./pages/topNav/TopNav";
 import Navbar from "./pages/navbar/Navbar";
 import Home from "./pages/home/Home";
+import Brend from "./pages/brend/Brend";
 export default function App() {
   return (
     <>
@@ -9,6 +10,7 @@ export default function App() {
     <TopNav/>
     <Navbar/>
     <Home/>
+    <Brend/>
     </BrowserRouter>
     </>
   )
---
+++ src/pages/brend/Brend.jsx
@@ -0,0 +1,15 @@
+export default function Brend() {
+    return (
+        <div className="bg-[#000000] px-4 py-10 flex flex-col gap-y-[22px]">
+            <div className="flex gap-x-[30px] items-center justify-between">
+                <a href=""><img src="/versace.svg" alt="brend icon" /></a>
+                <a href=""><img src="/zara.svg" alt="brend icon" /></a>
+                <a href=""><img src="/gucci.svg" alt="brend icon" /></a>
+            </div>
+            <div className="flex items-center justify-center gap-x-[31px]">
+                <a href=""><img src="/prada.svg" alt="brend icon" /></a>
+                <a href=""><img src="/c-k.svg" alt="brend icon" /></a>
+            </div>
+        </div>
+    )
+}
